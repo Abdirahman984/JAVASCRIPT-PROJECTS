69408c9ca4e0df27198116e9fd6ed1c635dbc8ec
Abdirahman Ali <abdirahmanalibakal0@gmail.com>
2026-03-03 10:26:36 +0300

    TODO-LIST APP

---
+++ 01_ToDo-App/script.js
@@ -0,0 +1,42 @@
+// SELECT THE ELEMENTS BY GETELMENT BY ID
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+// FUNCTION FOR ADDEVENTLISTENER WHEN CLICK DO SOMETHING IN THE INPUT 
+function addTask (){
+if(inputBox.value === '') {
+alert("you have to to write something")
+}else{
+let li = document.createElement("li");
+li.innerHTML= inputBox.value;
+listContainer.appendChild(li);
+
+// SPAN TO INSERT ICON DELET
+let span = document.createElement("span");
+span.innerHTML = '<i class="fa-solid fa-delete-left"></i>'
+li.appendChild(span);
+}
+inputBox.value = "";
+saveData ()
+}
+
+//  FUNCATION FOR ADDEVENTLISTNER CLICK DO THIS AND THIIS 
+listContainer.addEventListener("click", function(e){
+if(e.target.tagName === "LI"){
+ e.target.classList.toggle("checked");
+ saveData ()
+} else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+    saveData ()
+};
+})
+
+// SAVE THE DATA N THE BROWSER
+function saveData (){
+localStorage.setItem("data", listContainer.innerHTML); 
+}
+
+function showData (){
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showData();

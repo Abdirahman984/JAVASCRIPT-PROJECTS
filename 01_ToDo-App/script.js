342690c794e20b6b2e032cdf67c953ceb5b6556e
Abdirahman Ali <abdirahmanalibakal0@gmail.com>
2026-03-10 06:58:43 +0300

    Todo-List App

--- 01_ToDo-App/script.js
+++ 01_ToDo-App/script.js
@@ -1,19 +1,215 @@
+// SELECTING THE ELEMENTS GET ELEMENT BY ID
 const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const listContainer = document.getElementById("list-contanier");
 
-function addTask (){
- if(inputBox.value === '') {
+// FUNCTON FOR ADDEVENTLISTENER TO DISPLAY TASKS
+function taskDisplay (){
+  if(inputBox.value === ""){
     alert("you have to write something")
- }else {
-    let li = document.createElement("li");
-    li.innerHTML= inputBox.value;
+  } else {
+    const li = document.createElement("li")
+    li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
- }
- inputBox.value="";
- let span = document.createElement("span")
- span.innerHTML= '<i class="fa-solid fa-delete-left"></i>'
- li.appendChild(span);
+
+    const span = document.createElement("span")
+    span.innerHTML = '<i class="fa-solid fa-delete-left"></i>';
+    li.appendChild(span)
+  }
+  inputBox.value = "";
+  saveData ()
 }
+
+// ADDEVENTLISTENER FOR CHECKED TASK AND CANCEL
+listContainer.addEventListener("click", function (e){
+ if( e.target.tagName === "LI"){
+  e.target.classList.toggle ("checked");
+  saveData ()
+ } else if (e.target.tagName === "SPAN"){
+  e.target.parentElement.remove();
+ }
+ saveData ();
+})
+
+// FUNCTION FOR SAVNG DATA LOCAL STORAGE
+function saveData (){
+  localStorage.setItem ("data", listContainer.innerHTML);
+}
+
+// FUNCTION FOR DISPLAY DAT
+function displayData (){
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+displayData()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
